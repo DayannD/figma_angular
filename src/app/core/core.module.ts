bb69eadbfe18cceb5b8d885910a38468843eeb5d
dayann <dayanndeshayes@gmail.com>
2022-11-26 21:01:11 +0100

    save avant changement en scss

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UiModule } from '../ui/ui.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    FooterComponent,
+    NavbarComponent,
+    HeaderComponent,
+    UiModule,
   ]
+
 })
 export class CoreModule { }
